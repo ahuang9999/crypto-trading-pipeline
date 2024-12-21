34decd1af8ad20e3d6172ac599b5aa4b4e8dedd2
ahuang9999 <149345751+ahuang9999@users.noreply.github.com>
2024-12-21 02:30:18 -0500

    Update data_client.py

--- src/pysrc/data_client.py
+++ src/pysrc/data_client.py
@@ -1,12 +1,30 @@
+import requests, json
 class DataClient:
     def __init__(self):
-        raise NotImplementedError
+        self.timestamps = []
+        self.timestampsms = []
+        self.tids = []
+        self.prices = []
+        self.amounts = []
+        self.exchanges = []
+        self.types = []
 
     def _query_api(self) -> None:
-        raise NotImplementedError
+
+        base_url = "https://api.gemini.com/v1"
+        response = requests.get(base_url + "/trades/btcusd")
+        btcusd_trades = response.json()
+        
+        _parse_message(btcusd_trades)
 
     def _parse_message(self, message):
-        raise NotImplementedError
+        timestamps.append(message["timestamp"]);
+        timestampsms.append(message["timestampms"]);
+        tids.append(message["tid"]);
+        prices.append(message["price"]);
+        amounts.append(message["amount"]);
+        exchanges.append(message["exchange"]);
+        types.append(message["type"]);
 
     def get_data(self):
         raise NotImplementedError
