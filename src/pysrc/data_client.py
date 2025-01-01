83751041d1b566ef502d5d036eebb61aa689c84e
ahuang9999 <149345751+ahuang9999@users.noreply.github.com>
2025-01-01 18:19:52 -0500

    Update data_client.py

--- src/pysrc/data_client.py
+++ src/pysrc/data_client.py
@@ -18,19 +18,32 @@ class DataClient:
             base_url = "https://api.sandbox.gemini.com/v1"
         response = requests.get(base_url + "/trades/btcusd")
         btcusd_trades = response.json()
+        
         self._parse_message(btcusd_trades)
 
-    def _parse_message(self, message) -> None:
-        for dict in message:
-            self.timestamps.append(dict["timestamp"])
-            self.timestampsms.append(dict["timestampms"])
-            self.tids.append(dict["tid"])
-            self.prices.append(dict["price"])
-            self.amounts.append(dict["amount"])
-            self.exchanges.append(dict["exchange"])
-            self.types.append(dict["type"])
+    def _parse_message(self, message):
+        if isinstance(message,dict):
+            self.timestamps = message["timestamps"]
+            self.timestampsms = message["timestampsms"]
+            self.tids = message["tids"]
+            self.prices = message["prices"]
+            self.amounts = message["amounts"]
+            self.exchanges = message["exchanges"]
+            self.types = message["types"]
+        else:
+            for x in message:
+                if isinstance(x,str): break
+                self.timestamps.append(x['timestamp'])
+                self.timestampsms.append(x['timestampms'])
+                self.tids.append(x['tid'])
+                self.prices.append(x['price'])
+                self.amounts.append(x['amount'])
+                self.exchanges.append(x['exchange'])
+                self.types.append(x['type'])
+        
 
     def get_data(self, sandbox):
         self._query_api(sandbox)
         return {"timestamps":self.timestamps, "timestampsms":self.timestampsms, "tids":self.tids, "prices":self.prices, 
                "amounts":self.amounts, "exchanges":self.exchanges, "types":self.types}
+
