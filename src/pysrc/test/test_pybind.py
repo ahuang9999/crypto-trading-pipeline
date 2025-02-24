9ff69eb25b279978001f21044984ea7da16009d6
Alexander Huang <alexhuang240@gmail.com>
2025-02-24 15:38:39 -0600

    Wrote better tests

--- src/pysrc/test/test_pybind.py
+++ src/pysrc/test/test_pybind.py
@@ -1,28 +1,25 @@
 import pytest
 from pysrc import my_intern
-from pysrc.main import (
-    NTradesFeature,
-    PercentBuyFeature,
-    PercentSellFeature,
-    FiveTickVolumeFeature,
-)
 
 
 def test_ntrades() -> None:
-    assert NTradesFeature([(1, 1, False)]) == 1
-    assert NTradesFeature([(2, 1, False), (2, 2, True)]) == 2
+    NTrades_obj = my_intern.NTradesFeature()
+    assert NTrades_obj.compute_feature([(1, 1, False)]) == 1
+    assert NTrades_obj.compute_feature([(2, 1, False), (2, 2, True)]) == 2
 
 
 def test_pctbuy() -> None:
-    assert PercentBuyFeature([(1, 1, False)]) == 0
-    assert PercentBuyFeature([(1, 1, False), (1, 1, True)]) == 0.5
-    assert PercentBuyFeature([(1, 1, True)]) == 1
+    PctBuy_obj = my_intern.PercentBuyFeature()
+    assert PctBuy_obj.compute_feature([(1, 1, False)]) == 0
+    assert PctBuy_obj.compute_feature([(1, 1, False), (1, 1, True)]) == 0.5
+    assert PctBuy_obj.compute_feature([(1, 1, True)]) == 1
 
 
 def test_pctsell() -> None:
-    assert PercentSellFeature([(1, 1, False)]) == 1
-    assert PercentSellFeature([(1, 1, False), (1, 1, True)]) == 0.5
-    assert PercentSellFeature(
+    PctSell_obj = my_intern.PercentSellFeature()
+    assert PctSell_obj.compute_feature([(1, 1, False)]) == 1
+    assert PctSell_obj.compute_feature([(1, 1, False), (1, 1, True)]) == 0.5
+    assert PctSell_obj.compute_feature(
         [(1, 1, False), (1, 1, True), (1, 2, False)]
     ) == pytest.approx(0.67, rel=1e-2)
 
